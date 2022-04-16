1d27ff621187273565e80ed8236ebb6b676184b3
Nikhil <nickarsenal007@gmail.com>
2022-04-16 13:57:46 +0545

    Event call and useState hook

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [name, setName] = useState("Nick");
+  const handleClick = (e) => {
+    setName("Luigi");
+  };
   return (
     <div className="home">
       <h2>Home Page</h2>
+      <p>{name}</p>
+      <button onClick={(e) => handleClick(e)}>Click Me</button>
     </div>
   );
 };
