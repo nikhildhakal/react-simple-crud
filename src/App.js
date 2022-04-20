221463fd34652bc200711f17b88709e6b4022601
Nikhil <nickarsenal007@gmail.com>
2022-04-20 21:14:55 +0545

    Clear and blank app src

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
-import Navbar from "./Navbar";
-import Home from "./Home";
-import Create from "./Create";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import BlogDetail from "./BlogDetail";
+import "./App.css";
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <div className="content">
-          <Navbar />
-          <Routes>
-            <Route exact path="/" element={<Home />}></Route>
-            <Route path="/create" element={<Create />}></Route>
-            <Route exact path="/blogs/:id" element={<BlogDetail />}></Route>
-          </Routes>
-        </div>
-      </div>
-    </Router>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
